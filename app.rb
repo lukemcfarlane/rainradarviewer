0170424e76e312e0f88f19c6ef88fc0537e36766
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-06-02 20:32:54 +1200

    Added comments to ruby

--- app.rb
+++ app.rb
@@ -1,13 +1,22 @@
+# The main purpose of this Sinatra app is to get the data from
+# Metservice, transform it, and then return JSON data ready for
+# the AngularJS app to pick up and do something useful with it.
+#
+# Author: https://github.com/lukemcfarlane
+# Date:   June 2014
+
 BASE_URL = 'http://metservice.com'
 
 require 'sinatra'
 require 'json'
 require 'rest_client'
 
+# Serve the AngularJS app
 get '/' do
     send_file 'public/index.html'
 end
 
+# This is the endpoint used by the AngularJS app to get the radar image data
 get '/radar/images' do
     response = RestClient.get 'http://metservice.com/publicData/rainRadarChristchurch_2h_7min_300K'
 
