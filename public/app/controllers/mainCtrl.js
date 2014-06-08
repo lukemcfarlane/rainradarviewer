f85e6b9dcf24f8bc6333d51aa45dc6ae11ed930d
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-06-08 21:51:56 +1200

    First attempt at auto-refresh.. needs lots of improvement

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -4,11 +4,17 @@
  * @author https: //github.com/lukemcfarlane
  * @date    June 2014
  */
-app.controller('MainCtrl', function($scope, $http) {
+app.controller('MainCtrl', function($scope, $http, $interval) {
 	$scope.alerts = [];
 	$scope.numImages = 0;
 	$scope.activeIndex = 0;
 	$scope.images = [];
+	$scope.isUpdating = false;
+
+	var lastUpdatedTime = null;
+
+	$scope.lastUpdatedStr = '';
+
 
 	$scope.$watch('activeIndex', function(newVal, oldVal) {
 		_.each($scope.images, function(img) {
@@ -19,17 +25,34 @@ app.controller('MainCtrl', function($scope, $http) {
 		}
 	});
 
-	$http({
-		method: 'GET',
-		url: 'radar/images/'
-	}).success(function(data, status, headers, config) {
-		loadImages(data);
-	}).error(function(data, status, headers, config) {
-		$scope.alerts.push({
-			msg: 'Something went wrong while trying to retrieve rain radar images.',
-			type: 'danger'
+	var getNewImages = function() {
+		console.log('updating...');
+		$scope.isUpdating = true;
+		var endpoint;
+		if (lastUpdatedTime === null) {
+			endpoint = 'radar/images';
+		} else {
+			var since = parseInt(moment().format('X')) - ((new Date()).getTimezoneOffset() * 60); // ekkk... fix this soon!
+			endpoint = 'radar/images?since=' + since;
+		}
+		$http({
+			method: 'GET',
+			url: endpoint
+		}).success(function(data, status, headers, config) {
+			if (_.size(data) > 0) {
+				loadImages(data);
+				lastUpdatedTime = moment();
+				updateLastUpdatedStr();
+			}
+			$scope.isUpdating = false;
+		}).error(function(data, status, headers, config) {
+			$scope.alerts.push({
+				msg: 'Something went wrong while trying to retrieve rain radar images.',
+				type: 'danger'
+			});
+			$scope.isUpdating = false;
 		});
-	});
+	};
 
 	$scope.closeAlert = function(index) {
 		$scope.alerts.splice(index, 1);
@@ -45,14 +68,26 @@ app.controller('MainCtrl', function($scope, $http) {
 		img.active = true;
 	};
 
+	getNewImages();
+	$interval(getNewImages, 30 * 1000); // check for new data every 30 seconds
+
+	var updateLastUpdatedStr = function() {
+		if (lastUpdatedTime !== null) {
+			$scope.lastUpdatedStr = moment.duration(lastUpdatedTime.diff(moment())).humanize();
+		}
+	};
+
+	var getCurrentTime = function() {
+		return Math.round(new Date().getTime() / 1000);
+	};
+
 	var loadImages = function(imagesArr) {
+		console.log('Loading ' + imagesArr.length + ' new images...');
 		angular.forEach(imagesArr, function(img) {
 			img.id = _.uniqueId('img_');
 			img.active = false;
-			img.label = moment
-				.unix(img.datetime)
-				.tz('Pacific/Auckland')
-				.format('hh:mm a');
+			img.label = img.time;
+			img.retrievedAt = getCurrentTime();
 		});
 		imagesArr = _.sortBy(imagesArr, 'datetime');
 
